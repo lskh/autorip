3e0a513b3afe934d6ccb656539b26bf809ff6e81
Laurens Kils-Hütten <lkh@betola.de>
2015-10-06 14:35:11 +0200

    don't do autoripping when user is logged in

--- detectdisk.sh
+++ detectdisk.sh
@@ -4,11 +4,16 @@ export $ID_FS_LABEL
 
 {
   date >> /var/log/autorip.log
-  if [ $ID_CDROM_MEDIA -eq 1 ]; then
-	echo "Disk detected, starting autorip.sh" >> /var/log/autorip.log
-	/usr/local/bin/autorip.sh &	
-  else
-	echo "No Disk detected" >> /var/log/autorip.log
-  fi
+  if [ `who | wc -l` -gt 0 ] 
+    then
+       echo "User logged in. Aborting ..." >> /var/log/autorip.log
+    else
+ 	 if [ $ID_CDROM_MEDIA -eq 1 ]; then
+		echo "Disk detected, starting autorip.sh" >> /var/log/autorip.log
+		/usr/local/bin/autorip.sh &	
+  	else
+		echo "No Disk detected" >> /var/log/autorip.log
+  	fi
+    fi
 
 }
